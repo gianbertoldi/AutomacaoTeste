5a86dcdb0315cf71a0be71aa447c10a366bd8fc7
beatrizviegas <108840412+beatrizviegas@users.noreply.github.com>
2023-08-24 20:35:49 -0300

    Validar URL

--- src/test/java/com/br/automacaocob/AppTest.java
+++ src/test/java/com/br/automacaocob/AppTest.java
@@ -6,18 +6,22 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 
 public class AppTest 
 {
 	static WebDriver driver;
 
     @Test
-    public void shouldAnswerWithTrue()
+    public void validarUrl()
     {
         
         System.setProperty("webdriver.chrome.driver", "D:/User/Beatriz/Desktop/AutomacaoTeste/driver/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://www.google.com.br");
+        driver.get("https://tst.contatoseguro.io/pt/cob");
         driver.manage().window().maximize();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "https://tst.contatoseguro.io/pt/cob" );
     }
 }
