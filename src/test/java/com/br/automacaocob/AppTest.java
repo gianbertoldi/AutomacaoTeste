dc7d58dacb6cad08b999a13b903ce91eedaf09a6
beatrizviegas <108840412+beatrizviegas@users.noreply.github.com>
2023-08-24 22:01:31 -0300

    Validar idomas pronto

--- src/test/java/com/br/automacaocob/AppTest.java
+++ src/test/java/com/br/automacaocob/AppTest.java
@@ -1,10 +1,13 @@
 package com.br.automacaocob;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import junit.framework.Assert;
@@ -16,10 +19,9 @@ public class AppTest {
 	public static void setup() {
 		System.setProperty("webdriver.chrome.driver", "D:/User/Beatriz/Desktop/AutomacaoTeste/driver/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("https://tst.contatoseguro.io/pt/cob");
 		driver.manage().window().maximize();
 	}
-	@Test
+	/*@Test
 	public void validarUrl() {
 		driver.get("https://tst.contatoseguro.io/pt/cob");
 		String URL = driver.getCurrentUrl();
@@ -33,8 +35,19 @@ public class AppTest {
 		String textoTelefone = driver.findElement(By.xpath("/html/body/main/div[4]/div[2]/div/div")).getText();
 		Assert.assertEquals(telefone, "tel:0800 343 7231");
 		Assert.assertEquals(textoTelefone.contains("0800 343 7231"), true);
-	}
+	}*/
 	
+	@Test
+	public void validarIdioma() {
+		
+		driver.get("https://tst.contatoseguro.io/pt/cob");
+		WebElement button = driver.findElement(By.id("dropdownMenuButton"));
+		button.click();
+		String idiomaEspanhol = driver.findElement(By.xpath("//*[@id=\"navbar-dropdown\"]/ul/li[4]/div/div/a[2]/img")).getAttribute("title");
+		String idiomaPortugues = driver.findElement(By.xpath("//*[@id=\"navbar-dropdown\"]/ul/li[4]/div/div/a[1]/img")).getAttribute("title");
+		Assert.assertEquals(idiomaEspanhol, "Espanhol");
+		Assert.assertEquals(idiomaPortugues, "Português");
+	}
 	@AfterClass
 	public static void finaliza() throws InterruptedException{
 		Thread.sleep(3000);
